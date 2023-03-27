b3f6056478d6628a4ea9592540440d2331c19e4f
razo45 <razo12321@gmail.com>
2023-03-28 01:11:24 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MpdaTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MpdaTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
